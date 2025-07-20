246b05865ebc8f40eb124a20f7f0719e6ae3e40b
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2025-07-21 00:31:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,24 @@ import reviewRoutes from "./routes/reviewRouter.js";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: "https://recipe-master-flame.vercel.app",  
-  credentials: true,  
-}));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://recipe-master-flame.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
